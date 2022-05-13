574320efcde4da04808cf09d808bf7187a100977
Bianca Aguiar <biancaaguiar2021@gmail.com>
2022-05-13 20:41:04 -0300

    pesquisa (undefined) mas estilizada

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,5 @@
-const baseUrl = "https://el-geladon-backend-by-ip-production.up.railway.app/paletas";
+const baseUrl =
+  "https://el-geladon-backend-by-ip-production.up.railway.app/paletas";
 
 async function findAllPaletas() {
   const response = await fetch(`${baseUrl}/find-paletas`);
@@ -9,19 +10,39 @@ async function findAllPaletas() {
       "beforeend",
       `<div class="PaletaListaItem">
         <div>
-            <div class="PaletaListaItem__sabor">${paleta.sabor}</div>
-            <div class="PaletaListaItem__preco">R$ ${paleta.preco.toFixed(
+            <div class="PaletaListaItem_sabor">${paleta.sabor}</div>
+            <div class="PaletaListaItem_preco">R$ ${paleta.preco.toFixed(
               2
             )}</div>
-            <div class="PaletaListaItem__descricao">${paleta.descricao}</div>
+            <div class="PaletaListaItem_descricao">${paleta.descricao}</div>
           </div>
-            <img class="PaletaListaItem__foto" src=${
+            <img class="PaletaListaItem_foto" src=${
               paleta.foto
             } alt=${`Paleta de ${paleta.sabor}`} />
         </div>`
     );
   });
+}
+
+async function findPaletaById() {
+  const id = document.getElementById("idPaleta").value;
+  const response = await fetch(`${baseUrl}/find-paleta/${id}`);
+  const paleta = await response.json();
+  console.log(response)
+  console.log(paleta)
+
+  const paletaEscolhidaDiv = document.getElementById("paletaEscolhida");
+
+  paletaEscolhidaDiv.innerHTML = `<div class="PaletaCardItem">
+    <div>
+      <div class="PaletaCardItem__sabor">${paleta.sabor}</div>
+      <div class="PaletaCardItem__preco">R$ ${paleta.preco}</div>
+      <div class="PaletaCardItem__descricao">${paleta.descricao}</div>
+    </div>
+      <img class="PaletaCardItem__foto" src=${
+        paleta.foto
+      } alt=${`Paleta de ${paleta.sabor}`} />
+  </div>`;
 };
 
 findAllPaletas();
-
